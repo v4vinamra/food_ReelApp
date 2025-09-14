8488125c9450d9c234530c0af84e909da4b85058
v4vinamra <v4vinamra@gmail.com>
2025-09-15 02:09:39 +0530

    cors issue fix

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
 const foodRoutes = require('./routes/food.routes');
+
+require("dotenv").config();
+
 const foodPartnerRoutes = require('./routes/food-partner.routes');
 const cors = require('cors');
 
--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const ImageKit = require("imagekit");
 
 const imagekit = new ImageKit({
